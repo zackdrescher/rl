c2d968963fa74b4d3c8c0a27b16e375ba7bb92e0
Zack Drescher <zackdrescher@Zacks-MacBook-Pro.local>
2018-10-20 13:41:26 -0400

    Added observer and model arguemnt to episode

--- rl.py
+++ rl.py
@@ -7,11 +7,8 @@ from keras.layers import Dense, Flatten
 
 DEFAULT_ENV = 'CartPole-v0'
 
-def episode(agent = None, env = None, render = False):
-
-    """Runs an episode of the agent"""
-
-    # TODO: Agent argument
+def episode(model, env = None, render = False, epsilon = 0):
+    """Runs an episode of the sumulation with the given Q model"""
 
     if env is None:
         env = gym.make(DEFAULT_ENV)
@@ -21,7 +18,6 @@ def episode(agent = None, env = None, render = False):
     obs = np.expand_dims(observation, axis=0) 
     state = np.stack((obs, obs), axis=1)
 
-
     D = deque() 
     done = False
     
@@ -29,7 +25,11 @@ def episode(agent = None, env = None, render = False):
         
         env.render()
 
-        action = env.action_space.sample()
+        if np.random.rand() <= epsilon:
+            action = np.random.randint(0, env.action_space.n, size=1)[0]
+        else:
+            Q = model.predict(state)          # Q-values predictions
+            action = np.argmax(Q)  
 
         observation_new, reward, done, info = env.step(action) # take a random action
         
@@ -41,6 +41,47 @@ def episode(agent = None, env = None, render = False):
 
         if done:
            break
+
+    env.close()
+
+    return D
+
+def observe(num_obs, model, env = None, render = False, epsilon = 0):
+    """Runs an episode of the sumulation with the given Q model"""
+
+    if env is None:
+        env = gym.make(DEFAULT_ENV)
+
+    observation =  env.reset()
+
+    obs = np.expand_dims(observation, axis=0) 
+    state = np.stack((obs, obs), axis=1)
+
+    D = deque() 
+    done = False
+    
+    for _ in range(num_obs):
+        
+        env.render()
+
+        if np.random.rand() <= epsilon:
+            action = np.random.randint(0, env.action_space.n, size=1)[0]
+        else:
+            Q = model.predict(state)          # Q-values predictions
+            action = np.argmax(Q)  
+
+        observation_new, reward, done, info = env.step(action) # take a random action
+        
+        obs_new = np.expand_dims(observation_new, axis=0)          # (Formatting issues)
+        state_new = np.append(np.expand_dims(obs_new, axis=0), state[:, :1, :], axis=1) 
+
+        D.append((state, action, reward, state_new, done))         # 'Remember' action and consequence
+        state = state_new  
+
+        if done:
+            env.reset()           # Restart game if it's finished
+            obs = np.expand_dims(observation, axis=0)     # (Formatting issues) Making the observation the first element of a batch of inputs 
+            state = np.stack((obs, obs), axis=1)
 
     env.close()
 
@@ -66,15 +107,9 @@ if __name__ == '__main__':
 
     model = initialize_model(env.observation_space, env.action_space)
 
-    D = episode(env = env, render= True)
+    D = episode(model, env = env, render= True)
 
-    state, action, reward, state_new, done = D[0]
-
-    inputs_shape = (1,) + state.shape[1:]
-    inputs = np.zeros(inputs_shape)
-    targets = np.zeros((1, env.action_space.n))
-
-    inputs[0:1] = np.expand_dims(state, axis=0)
+    D2 = observe(1000, model, env = env, render= True)
 
 
 

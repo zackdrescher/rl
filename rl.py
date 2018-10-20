72530e88ac37a633d9e7d97f55724a28330fc4d8
Zack Drescher <zackdrescher@Zacks-MacBook-Pro.local>
2018-10-20 14:43:16 -0400

    add learning

--- rl.py
+++ rl.py
@@ -4,11 +4,15 @@ import numpy as np
 import random
 from keras.models import Sequential    
 from keras.layers import Dense, Flatten 
+# Just disables the warning, doesn't enable AVX/FMA
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 DEFAULT_ENV = 'CartPole-v1'
 
 def episode(model, env = None, render = False, epsilon = 0):
-    """Runs an episode of the sumulation with the given Q model"""
+    """Runs an episode of the sumulation with the given Q model
+    Epsilon is the probability of doing a random move"""
 
     if env is None:
         env = gym.make(DEFAULT_ENV)
@@ -107,15 +111,56 @@ def initialize_model(obs_space, action_space):
     print("Model Initialized")
     return model
 
+def learn(model, D, mini_batch_size = None):
+
+    print("Learning...")
+
+    if mini_batch_size is None:
+        mini_batch_size = len(D)
+
+    batch = random.sample(D, mini_batch_size)                              # Sample some moves
+
+    X, Y = bellman(batch, model)
+
+    # Train network to output the Q function
+    model.train_on_batch(X, Y)
+
+    print("Learning complete")
+    
+def bellman(D, model, gamma = 0.9):
+
+    m = len(D)
+
+    state = D[0][0]
+    inputs_shape = (m,) + state.shape[1:]
+    inputs = np.zeros(inputs_shape)
+    targets = np.zeros((m, model.output_shape[1]))
+
+    for i, (state, action, reward, state_new, done) in enumerate(D):
+        
+        # Build Bellman equation for the Q function
+        inputs[i:i+1] = np.expand_dims(state, axis=0)
+        targets[i] = model.predict(state)
+        Q_sa = model.predict(state_new)
+        
+        if done:
+            targets[i, action] = reward
+        else:
+            targets[i, action] = reward + gamma * np.max(Q_sa)
+
+    return inputs, targets
+
 if __name__ == '__main__':
 
     env = gym.make(DEFAULT_ENV)
 
     model = initialize_model(env.observation_space, env.action_space)
 
-    D = episode(model, env = env, render= True)
+    D = observe(1000, model, env = env, render= True, epsilon=1)
 
-    D2 = observe(1000, model, env = env, render= True)
+    learn(model, D)
+
+    D2 = observe(1000, model, env=env, render=True)
 
 
 
